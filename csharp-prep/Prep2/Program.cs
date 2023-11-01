eea38e8fe2787b12e87762729936d57befb0e567
niwemu1 <niwemugenideborah@gmail.com>
2023-11-01 12:53:28 -0700

    Finished w1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+
+        string answer = Console.ReadLine();
+
+        int percent = int.Parse(answer);
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Stretch Challenge 
+        string sign = "";
+        int number = int.Parse(answer);
+        number = percent % 10;
+
+        if (number >= 7)
+        {
+             sign = "+";
+        }
+        else if (number < 3)
+        {
+             sign = "-";
+        }
+        else
+        {
+              sign = "";
+        }
+
+    
+        if (percent >= 91)
+        {
+              sign = "";
+        }
+
+    
+        if (letter == "F")
+        {
+              sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Good luck next time!");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        
+        int magicNumber = randomGenerator.Next(1, 101);
+        Console.WriteLine("What is the magic number? ");
+        magicNumber = int.Parse(Console.ReadLine());
+
+        int guess = -1;
+
+        // We could also use a do-while loop here...
+        while (guess != magicNumber)
+        {
+          
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
