bf3a5db5bf6227b285c9681f9c99efbe1326c6a6
niwemu1 <niwemugenideborah@gmail.com>
2023-11-08 10:10:47 -0800

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -30,7 +30,7 @@ class Program
             }
             else
             {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine("You guessed it!!");
             }
 
         }                    
